402ad9204c0ef0ca68f933e99a1f3eadcc5e1ab2
shangxin <shangxin@outlook.com>
2026-02-28 20:51:51 +0800

    feat: add a2a client barrel exports and unit tests

---
+++ src/core/protocols/a2a/client/index.ts
@@ -0,0 +1,4 @@
+export { createA2AClient } from './client.js';
+export { createA2AHttpTransport } from './http-transport.js';
+export type { A2AClientTransport } from './transport.js';
+export type { A2AJsonRpcRequest, A2AOutboundAction, A2ATaskResult } from './types.js';
